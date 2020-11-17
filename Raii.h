5865521924df81ee65067eb0ea0bbafe9dda8945
drem1lin <drem1lin@gmail.com>
2020-11-17 09:11:40 +0300

    Комментарии о критической секции

--- Raii.h
+++ Raii.h
@@ -2,25 +2,25 @@
 #include <iostream>
 #include "windows.h"
 
-class Resource
+class Resource // TODO что за название, где final, const, noexcept
 {
 public:
-	Resource(CRITICAL_SECTION* section)
+	Resource(CRITICAL_SECTION* section)  //TODO а почему ты только две функции реализовал в cpp? либо все здесь, либо все туда. 
 	{
-		m_res_section = *section;
+		m_res_section = *section; //TODO херню сделал. Зачем разименовал
 		lock();		
 	}
 	~Resource() 
 	{
 		unlock();
 	}
-	void lock();
-	void unlock();
+	void lock(); //TODO Именование функций
+	void unlock(); //Лишние функции
 private:
 	CRITICAL_SECTION m_res_section;
 };
 
-class Raii
+class Raii // TODO что за название
 {
 public:
 	Raii()
@@ -32,10 +32,19 @@ public:
 		DeleteCriticalSection(&m_section);
 	}
 
-	void lock()
+	//operator LPCRITICAL_SECTION() {}; //TODO где?
+
+	void lock() //Лишняя функция и не правильная. При вызове lock берется и сразу отпускается Resource
 	{
 		Resource res(&m_section);
 	}
 private:
 	CRITICAL_SECTION m_section;
 };
+
+/*  Ты в классе хранишь свою обертку над критической секцией - ты это сделал
+	Там, где надо провести синхронную операцию, ты должен вызывать обертку над операциями 
+	блокировки - которая заблокирует, а потом разблокирует секцию
+
+	Resource - это твоя обертка блокировки.
+	*/
